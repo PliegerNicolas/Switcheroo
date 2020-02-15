7494420778fc5a05f91e9237934ad784e161836f
Nicolas Plieger <plieger05@gmail.com>
2020-02-15 16:49:26 +0000

    Making uploading images through cloudinary possible

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,6 +2,7 @@ class Instrument < ApplicationRecord
   INSTRUMENT_STATUS = %w[rented available]
   belongs_to :user
   has_many :favorites
+  has_many_attached :photos
   validates :name, :price, :latitude, :longitude, :status, :views, :user_id, presence: true
   validates :status, inclusion: { in: INSTRUMENT_STATUS }
 end
