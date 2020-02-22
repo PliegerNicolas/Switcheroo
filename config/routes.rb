c7b3d0824d4ac963c1b3c4d6a0f8dc048b2333a6
Nuno Martins <nuno@alvesmartins.eu>
2020-02-22 16:40:39 +0000

    Add route to see your profile dashboard with listing of orders and instruments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :instruments do
     resources :orders, only: [:new]
   end
+
+  get '/profile', to: 'users#profile'
   devise_for :users
   resources :users
 end
