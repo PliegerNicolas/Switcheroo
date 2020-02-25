1583234e36783e92797254c6d0d46406c5952da6
Nuno Martins <nuno@alvesmartins.eu>
2020-02-25 21:02:13 +0000

    Fix the seed and add some testing users

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,8 +1,8 @@
 class Instrument < ApplicationRecord
   INSTRUMENT_STATUS = %w[rented available pending]
   belongs_to :user
-  has_many :favorites
-  has_many :orders
+  has_many :favorites, dependent: :destroy
+  has_many :orders, dependent: :destroy
   has_many_attached :photos
   validates :name, :price, :status, :views, :user_id, presence: true
   validates :status, inclusion: { in: INSTRUMENT_STATUS }
